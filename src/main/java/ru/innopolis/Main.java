2bebfc98996ddbf71bcfbe7cb0f8352a902b11f6
Maksim Gryaznov <mvgryaznov.sbt@sberbank.ru>
2017-05-21 13:44:03 +0300

    first commit

---
+++ src/main/java/ru/innopolis/Main.java
@@ -0,0 +1,19 @@
+package ru.innopolis;
+
+import java.io.File;
+
+public class Main {
+    private static final String OUTPUT_DIRECTORY = "/Users/sber-mac/IdeaProjects/dirscanner/dirscanner_output";
+
+    public static void main(String[] args) {
+        File folder = new File(OUTPUT_DIRECTORY);
+        File[] folderEntries = folder.listFiles();
+        //чтение каждого файла в отдельном потоке
+        for (File entry : folderEntries) {
+            if (!entry.isDirectory()) {
+                ReaderThread thread = new ReaderThread(entry);
+                thread.start();
+            }
+        }
+    }
+}
---
+++ src/main/java/ru/innopolis/ReaderThread.java
@@ -0,0 +1,73 @@
+package ru.innopolis;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ReaderThread extends Thread {
+    private File inputFile;
+
+    public ReaderThread(File inputFile) {
+        this.inputFile = inputFile;
+    }
+
+    @Override
+    public void run() {
+        //чтение
+        Map<String, String> messagesMap = new ConcurrentHashMap<>();
+        StringBuilder sb = new StringBuilder();
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(
+                new FileInputStream(inputFile), "UTF-8"))) {
+//            System.out.println(file.getName());
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                sb.append(inputLine);
+                //ищем первое вхождение пробела - разделителя целевого каталога получателя и текста для получателя
+                int index = inputLine.indexOf(" ");
+                String path = inputLine.substring(0, index).concat("/").concat(inputFile.getName());
+                String message = inputLine.substring(index + 1);
+                //записываем получателя и сообщение в мап, если его не существует
+                if (messagesMap.get(path) == null) {
+                    messagesMap.put(path, message);
+                } else {
+                    // в противном случае прикрепляем сообщение к получателю
+                    String msg = messagesMap.get(path);
+                    messagesMap.put(path, message.concat(";").concat(msg));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        //создание
+        Map.Entry entry;
+        Iterator iterator = messagesMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            entry = (Map.Entry) iterator.next();
+            //создание файла
+            File outputFile = new File(entry.getKey().toString());
+            if (!outputFile.exists()) {
+                outputFile.getParentFile().mkdirs();
+                try {
+                    outputFile.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            //запись в файл
+            try (Writer out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(outputFile), "UTF-8"))) {
+                out.write(entry.getValue().toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
+
+
